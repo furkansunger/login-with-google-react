4bd7c38e6add14c34325efcc584c17634463549b
Furkan Sünger <furkan.sunger@outlook.com>
2024-01-25 14:33:21 +0300

    Server and routes created

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+import githubRoutes from './routes/github-routes';
+import googleRoutes from './routes/google-routes';
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(
+    cors({
+        origin: ['http://localhost:5173'],
+        methods: 'GET, POST',
+    }),
+);
+
+app.use(express.json());
+
+app.use('/api/github', githubRoutes);
+app.use('/api/google', googleRoutes);
+
+app.listen(PORT, () => console.log('Server: ', PORT));
